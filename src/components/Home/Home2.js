659520cd1761a0cafcf0596100f4198c594fc74a
adityaS-123 <126447180+adityaS-123@users.noreply.github.com>
2025-03-05 12:59:31 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -71,7 +71,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/adityaa04"
+                  href="https://www.instagram.com/_adityaa04"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
